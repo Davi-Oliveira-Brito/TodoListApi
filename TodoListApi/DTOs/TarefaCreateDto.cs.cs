e06954ed06b22289bc66ee2361274f1b26af2eff
Davi Oliveira Brito <davioliveirabrito07@gmail.com>
2026-07-01 14:15:31 -0300

    feat: adiciona DTOs de Tarefa

---
+++ TodoListApi/DTOs/TarefaCreateDto.cs.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoListApi.DTOs
+{
+    public class TarefaCreateDto
+    {
+        public string Titulo { get; set; } = string.Empty;
+        public string Descricao { get; set; } = string.Empty;
+        public int TipoTarefaId { get; set; }
+    }
+}
---
+++ TodoListApi/DTOs/TarefaResponseDto.cs
@@ -0,0 +1,13 @@
+﻿namespace TodoListApi.DTOs
+{
+    public class TarefaResponseDto
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; } = string.Empty;
+        public string Descricao { get; set; } = string.Empty;
+        public bool Concluida { get; set; }
+        public DateTime DataInclusao { get; set; }
+        public int TipoTarefaId { get; set; }
+        public string TipoTarefaDescricao { get; set; } = string.Empty;
+    }
+}
