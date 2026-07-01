9841dff5e7222efbf51567a96dcbb07fddfd02ce
Davi Oliveira Brito <davioliveirabrito07@gmail.com>
2026-07-01 14:14:51 -0300

    feat: adiciona models TipoTarefa e Tarefa

---
+++ TodoListApi/Models/Tarefa.cs
@@ -0,0 +1,14 @@
+﻿namespace TodoListApi.Models
+{
+    public class Tarefa
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; } = string.Empty;
+        public string Descricao { get; set; } = string.Empty;
+        public bool Concluida { get; set; } = false;
+        public DateTime DataInclusao { get; set; } = DateTime.Now;
+
+        public int TipoTarefaId { get; set; }
+        public TipoTarefa? TipoTarefa { get; set; }
+    }
+}
---
+++ TodoListApi/Models/TipoTarefa.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoListApi.Models
+{
+    public class TipoTarefa
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; } = string.Empty;
+        public DateTime DataInclusao { get; set; } = DateTime.Now;
+    }
+}
